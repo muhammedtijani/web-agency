94d97909809ffefd4b0759e6c2734928a81567e9
Muhammed Shittu <coolzyte@gmail.com>
2022-03-02 11:28:23 +0100

    fix: count section

--- src/components/Count/count.js
+++ src/components/Count/count.js
@@ -5,48 +5,54 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
   border-bottom: 1px solid rgba(0, 0, 0, 0.05);
   .container {
-    width: 1450px;
     display: flex;
-    @media screen and (max-width: 640px) {
-      width: 100%;
-      flex-wrap: wrap;
-    }
-  }
-  .home-count-item {
-    flex: 0 0 25%;
-    flex-grow: 0;
-    flex-shrink: 0;
-    flex-basis: 25%;
-    max-width: 25%;
-    margin-bottom: 25px;
-    padding-right: 25px;
-    padding-left: 25px;
-    border-right: 1px solid rgba(0, 0, 0, 0.05);
-    &:last-child,
-    &:only-child {
-      border-right: none;
-    }
-    .holder {
-      .number {
-        margin-bottom: 3px;
-        font-weight: bold;
-        font-size: 36px;
-        color: #000;
-        line-height: 0.9;
-        @media screen and (max-width: 650px) {
-          font-size: 24px;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding-top: 25px;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+
+    .home-count-item {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-grow: 0;
+      flex-shrink: 0;
+      margin-bottom: 25px;
+
+      border-right: 1px solid rgba(0, 0, 0, 0.05);
+      &:last-child,
+      &:only-child {
+        border-right: none;
+      }
+
+      flex-basis: 50%;
+      max-width: 50%;
+
+      .holder {
+        padding-right: 25px;
+        padding-left: 25px;
+        .number {
+          margin-bottom: 3px;
+          font-weight: bold;
+          font-size: 36px;
+          color: #000;
+          line-height: 0.9;
+        }
+        p {
+          font-size: 18px;
         }
       }
     }
-    @media screen and (max-width: 650px) {
-      flex-basis: 50%;
-      max-width: 50%;
-      p {
-        font-size: 14px;
-      }
+
+    @media screen and (min-width: 990px) {
+      flex-direction: row;
+      flex-wrap: nowrap;
     }
   }
 `;
